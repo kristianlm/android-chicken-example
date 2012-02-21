761e6ee72a41f7836af0b49452fa6be12401d8aa
Kristian Lein-Mathisen <kristianlein@gmail.com>
2012-02-21 07:02:20 +0100

    refactor, async-input: argument async-input-port->port, let ([]) format

--- sdcard/async-input.scm
+++ sdcard/async-input.scm
@@ -3,40 +3,42 @@
 (define-record async-input-port input-port continuation)
 
 (set! RETURN #f)
-(define (new-async-input-port input-port proc)
+(define (new-async-input-port input-port thunk)
   (define new-aip (make-async-input-port input-port #f))
   (define reset-proc
     (lambda (ret)
       (begin
-        (let [(final-return-value (proc))]
+        (let [(final-return-value (thunk))]
           (async-input-port-continuation-set! new-aip reset-proc)
           (RETURN final-return-value)))))
   (async-input-port-continuation-set! new-aip
                                       reset-proc)
   new-aip)
 
-(define (read-async-input-port async-input-port)
-  (assert (async-input-port? async-input-port))
-  (assert (procedure? (async-input-port-continuation async-input-port)))
+(define (read-async-input-port port)
+  (assert (async-input-port? port))
+  (assert (procedure? (async-input-port-continuation port)))
 
   (call/cc
    (lambda (return)
      (set! RETURN return)
-     (let* [(my-read
+     (let* ([my-read
              (lambda ()
                (call/cc
-                (lambda (cc) (async-input-port-continuation-set! async-input-port cc)))
-               (if (char-ready? i )
-                   (read-char i)
+                (lambda (cc) (async-input-port-continuation-set! port cc)))
+               (if (char-ready? (async-input-port-input-port port))
+                   (read-char (async-input-port-input-port port))
                    (begin
-                     (RETURN #f )))))    ; not ready jet
+                     (RETURN #f ))))]  ; not ready yet
             
-            (my-ready (lambda () (char-ready? i)))
-            (my-close (lambda () (close-input-port i)))
-            (p (make-input-port
+            [my-ready (lambda () (char-ready? (async-input-port-input-port port)))]
+            [my-close (lambda () (close-input-port (async-input-port-input-port port)))]
+            [p (make-input-port
                 my-read
                 my-ready
-                my-close))]
+                my-close)])
        (with-input-from-port p
          (lambda () 
-           ((async-input-port-continuation async-input-port) #f) ))))))
+           ((async-input-port-continuation port) #f) ))))))
+
+
