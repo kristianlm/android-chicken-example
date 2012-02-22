07be307c2a7a36d71e0e7e5430f7967f57834cf8
Kristian Lein-Mathisen <kristianlein@gmail.com>
2012-02-22 05:31:51 +0100

    remote-repl: bugfix, now error-handling etc works with coroutines

--- sdcard/repl.scm
+++ sdcard/repl.scm
@@ -1,21 +1,25 @@
 
 
 (define (my-repl i o)
-  (let* ([show-prompt
-          (let ([line-number 0])
-            (lambda ()
-              (set! line-number (fx+ 1 line-number))
-              (display (conc "@" line-number "> ") o)))])
-    (handle-exceptions
-     exn
-     (with-output-to-port o
-       (lambda () 
-         (print-error-message exn)
-         (print-call-chain)))
-     (let loop ()
+  (let ([show-prompt
+         (let ([line-number 0])
+           (lambda ()
+             (set! line-number (fx+ 1 line-number))
+             (display (conc "@" line-number "> ") o)))])
+    (let loop ()
+      (handle-exceptions
+       exn
+       (begin (handle-exceptions
+               nested-exp
+               (print-error-message nested-exp)
+               (with-output-to-port o
+                 (lambda () 
+                   (print-error-message exn)
+                   (print-call-chain)
+                   (loop)))))
        (show-prompt)
        (let [(sexp (read i))]
-;         (logi (conc "eval: " sexp))
+         (logi (conc "eval: " sexp))
          (with-output-to-port o
            (lambda ()
              (with-error-output-to-port o
@@ -23,3 +27,4 @@
                                           (display (eval sexp))
                                           (write-char #\newline))))))
        (loop)))))
+
